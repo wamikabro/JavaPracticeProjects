bb3e2bcc96dafb5071b25927bf1589ee0da5d794
Wamique Abro <wamik.abro212@gmail.com>
2023-09-21 19:48:46 +0500

    Project Passing by Value. Started MemoryManagement

---
+++ Getting Deeper/src/MemoryManagement/PassingByValue.java
@@ -0,0 +1,19 @@
+package MemoryManagement;
+
+public class PassingByValue {
+    public static void main(String[] args) {
+        // initializing local value to 5
+        int localValue = 5;
+
+        // passing by value to the method: passing copy of the actual value
+        calculate(localValue);
+
+        // it will print same 5 because the method had its own copy of value
+        // keeping the real value unchanged.
+        System.out.println(localValue);
+    }
+
+    public static void calculate(int calcValue){
+        calcValue = calcValue * 100;
+    }
+}
