5f20bbc0a7c440a71a853c0e380f4c6d81fdbd6d
Wamique Abro <wamik.abro212@gmail.com>
2023-10-02 11:47:12 +0500

    Solution of Sending the immutable copy of collection to the caller to cover Escaping reference and save the programmer from confusion at the same time.

---
+++ Getting Deeper/src/MemoryManagement/EscapingReferences/ImmutableCollections/Main.java
@@ -0,0 +1,22 @@
+package MemoryManagement.EscapingReferences.ImmutableCollections;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerRecords records = new CustomerRecords();
+
+        records.addCustomer(new Customer("Moazzam"));
+
+        records.addCustomer(new Customer("Wamique"));
+
+        // now since the map is not mutable, this code is going to throw an exception on your face.
+        //records.getCustomer().clear();
+
+        // Now this is the best solution so far
+        // the summary to the case study is that we should return unmodifiable map or list whatever instead of returning reference.
+        for(Customer next: records.getCustomer().values()){
+            System.out.println(next);
+        }
+
+        // stay secure, stay safe! 
+    }
+}
