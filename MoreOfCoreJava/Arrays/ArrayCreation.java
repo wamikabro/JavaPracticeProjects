df423dfaec3058e59200675cc98ccb310fee5e55
Wamique Abro <wamik.abro212@gmail.com>
2023-10-06 20:58:23 +0500

    Basic Array Creation

---
+++ MoreOfCoreJava/Arrays/ArrayCreation.java
@@ -0,0 +1,19 @@
+package javaBeginnersGuideProjects.MoreOfCoreJava.Arrays;
+import java.util.Scanner;
+public class ArrayCreation {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter no of elements: ");
+        n = scanner.nextInt();
+
+        int a[] = new int[n];
+        System.out.println("Enter values");
+        for(int i=0; i<n; i++)
+            a[i] = scanner.nextInt();
+
+        System.out.println("Elements are: ");
+        for(int i=0; i<n; i++)
+            System.out.print(a[i]);
+    }
+}
