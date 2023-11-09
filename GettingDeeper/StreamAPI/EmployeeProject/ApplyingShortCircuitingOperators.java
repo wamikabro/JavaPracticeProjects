cb15ea7d9b1cc226dd4fc73d9804cae40854c7d3
Wamique Abro <wamik.abro212@gmail.com>
2023-11-09 13:26:34 +0500

    try short circuitng operatros in Stream API

---
+++ GettingDeeper/StreamAPI/EmployeeProject/ApplyingShortCircuitingOperators.java
@@ -0,0 +1,26 @@
+package javaBeginnersGuideProjects.GettingDeeper.StreamAPI.EmployeeProject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ApplyingShortCircuitingOperators {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee("Wamique", "Abro", 30000.0, List.of("Project 1", "Project 2")));
+        employees.add(new Employee("Wasique", "Abro", 40000.0, List.of("Project 2", "Project 3")));
+        employees.add(new Employee("Ahsan", "Abro", 25000.0, List.of("Project 3", "Project 4")));
+        employees.add(new Employee("Moazzam", "Abro", 30000.0, List.of("Project 4", "Project 5")));
+
+        // short circuit operations
+        List<Employee>  shortCircuit = employees
+                .stream()
+                .skip(1) // skip the first element
+                .limit(1) // just take the 1 element
+                .collect(Collectors.toList()); // collect the element into the list
+
+        // it will print the 2nd element only
+        System.out.println(shortCircuit);
+    }
+}
