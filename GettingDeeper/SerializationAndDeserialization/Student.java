26420d34637e971653626c8a4de98ba674163e67
Wamique Abro <wamik.abro212@gmail.com>
2023-11-15 10:42:04 +0500

    add student class so we can serialize and deserialize the object

---
+++ GettingDeeper/SerializationAndDeserialization/Student.java
@@ -0,0 +1,6 @@
+package javaBeginnersGuideProjects.GettingDeeper.SerializationAndDeserialization;
+import java.io.*;
+public class Student implements Serializable{
+    public int studentRollNo;
+    public String studentName;
+}
