e0a1f607ac3abddf030e2e2a6d58ab9a10ca5d2f
Wamique Abro <wamik.abro212@gmail.com>
2023-11-26 13:15:07 +0500

    First Project using Gradle

---
+++ BuildTools/Gradle/FirstGradleProject/src/main/java/GradleTutorial.java
@@ -0,0 +1,6 @@
+
+public class GradleTutorial {
+    public static void main(String[] args) {
+        System.out.println("Gradle 4 Wamique!");
+    }
+}
---
+++ BuildTools/Gradle/FirstGradleProject/src/test/java/GradleTutorial.java
@@ -0,0 +1,9 @@
+// this library needs to be imported
+// it is by default not in Java
+import org.junit.Test;
+public class GradleTutorial {
+    @Test
+    public void verifyNoExceptionThrown(){
+        GradleTutorial.main(new String[]{});
+    }
+}
