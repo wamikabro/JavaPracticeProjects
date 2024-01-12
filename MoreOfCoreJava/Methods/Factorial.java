ba8e29cf22b0ec2c4592d9005419ef52fca6bcf1
Wamique Abro <wamik.abro212@gmail.com>
2024-01-12 18:57:49 +0500

    correct factorial program.

--- MoreOfCoreJava/Methods/Factorial.java
+++ MoreOfCoreJava/Methods/Factorial.java
@@ -1,12 +1,13 @@
 public class Factorial {
     public static void main(String[] args) {
-        System.out.println(factorial(4));
+        System.out.println(factorial(5));
     }
 
     static int factorial(int number){
         int factorial = 1;
 
-        for(int i = number; i >= 1; i--, factorial*=number);
+        for(int i = 1; i <= number; i++)
+            factorial *= i;
 
         return factorial;
     }
